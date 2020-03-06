abc558a065286cab580bde0471ba394ae5c88990
FrixaCoder <37442533+FrixaCoder@users.noreply.github.com>
2020-03-06 19:40:13 +0500

    added more programming languages

--- src/Data/words.js
+++ src/Data/words.js
@@ -1,2 +1,15 @@
-export const words = ["java", "javascript", "cplusplus", "python"]
+export const words = [
+  "java",
+  "javascript",
+  "cplusplus",
+  "python",
+  "dart",
+  "kotlin",
+  "csharp",
+  "c",
+  "php",
+  "go",
+  "r",
+  "ruby"
+]
 export const generateRandomWord = () => words[Math.floor(Math.random() * 4)]
