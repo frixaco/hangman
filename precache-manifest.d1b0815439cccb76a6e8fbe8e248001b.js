93248d72f34dd40f787c56054535016c0f3fbaaa
frixaco <rustam21ashurmatov@gmail.com>
2020-08-03 21:36:33 +0500

    deploy to gh-pages

---
+++ precache-manifest.d1b0815439cccb76a6e8fbe8e248001b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad289fe00fa0a2a7e71a8b3d8af0d65b",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "6fb75bf7be802ff963a5",
+    "url": "/hangman/static/css/main.75a21624.chunk.css"
+  },
+  {
+    "revision": "d002f0b72055c8bf6fc3",
+    "url": "/hangman/static/js/2.c7ae7da8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hangman/static/js/2.c7ae7da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb75bf7be802ff963a5",
+    "url": "/hangman/static/js/main.ee80cfd8.chunk.js"
+  },
+  {
+    "revision": "889658a6d1efeb2059b1",
+    "url": "/hangman/static/js/runtime-main.c4be31dd.js"
+  }
+]);
